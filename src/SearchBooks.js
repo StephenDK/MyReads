87e212c1a736971c1412d8b5ac98f81b8ba0ad3c
StephenDK <stephendklein08@gmail.com>
2019-01-26 20:59:04 -0800

    Search component

---
+++ src/SearchBooks.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class SearchBooks extends Component {
+    state = {
+        query: ''
+    }
+
+
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default SearchBooks;
